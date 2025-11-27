03bcac30365c28e4653ed19e2a12d0930eefe6c6
Aakash Thakur <aakash.thakur@sharajman.com>
2025-11-27 12:32:56 +0530

    Added a basic get and post api in 08.

--- 08 Demo Api using Express/index.js
+++ 08 Demo Api using Express/index.js
@@ -1,10 +1,92 @@
 const express = require("express");
-const http = require("http");
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
+app.use(express.json());
 
+const UserRecordFilePath = path.join(__dirname, "records", "UserRecords.txt");
 
-app.listen(3000,()=>{
+app.get("/list-user", (req, res) => {
+    const userList = fs.readFile(UserRecordFilePath, (error, data) => {
+        if (error) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                status: false,
+                code: StatusCodes.BAD_REQUEST,
+                message: ReasonPhrases.BAD_REQUEST,
+            });
+        }
+
+        const dataReadable = data?.toString();
+        const row = dataReadable?.split("\n") ?? [];
+        var resArr = [];
+        for (var i = 0; i < row.length; i++) {
+            if (i !== row.length - 1) {
+                const responseArr = row[i]?.split("<>");
+                resArr?.push({
+                    Name: responseArr[0],
+                    Age: +responseArr[1]
+                })
+            }
+        }
+
+        return res.status(StatusCodes.OK).json({
+            status: true,
+            code: StatusCodes.OK,
+            message: ReasonPhrases.OK,
+            data: {
+                saved: resArr,
+            },
+        });
+
+    })
+});
+
+app.post("/add-user", (req, res) => {
+    const payload = req.body;
+
+    if (!payload?.Name || !payload?.Age) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            status: false,
+            code: StatusCodes.BAD_REQUEST,
+            message: ReasonPhrases.BAD_REQUEST,
+        });
+    }
+
+    const line = `${payload.Name}<>${payload.Age}\n`;
+    fs.appendFile(UserRecordFilePath, line, (error) => {
+        if (error) {
+            console.error("Error writing to file:", error);
+
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                status: false,
+                code: StatusCodes.INTERNAL_SERVER_ERROR,
+                message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+                data: {
+                    not_saved: {
+                        Name: payload.Name,
+                        Age: payload.Age,
+                    },
+                },
+            });
+        }
+
+        return res.status(StatusCodes.OK).json({
+            status: true,
+            code: StatusCodes.OK,
+            message: ReasonPhrases.OK,
+            data: {
+                saved: {
+                    Name: payload.Name,
+                    Age: payload.Age,
+                },
+            },
+        });
+    });
+});
+
+app.listen(3000, () => {
     console.log("-----server is running-----");
-})
+});
